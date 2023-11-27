a401cb2b17ac906068a764e3fae26cb0d8cd9c69
C Karthikesavan <karthikesavan2049@gmail.com>
2023-11-27 23:46:53 +0530

    created single inheritence program

---
+++ OOPS/Inheritence/SingleInheritence.java
@@ -0,0 +1,31 @@
+// Here, the attributes from SampleClass are inherited to SingleInheritence class
+
+public class SingleInheritence extends SampleClass{
+    public static void main(String[] args) {
+        SampleClass object = new SampleClass();
+        object.LearningJava();
+        object.PractisingJava();
+        object.TeachJava();
+    }
+}
+
+class SampleClass{
+    void LearningJava(){
+        System.out.println("I am learning Java");
+    }
+
+    void PractisingJava(){
+        System.out.println("I am practising Java");
+    }
+
+    void TeachJava(){
+        System.out.println("Tomorrow, I will teach Java");
+    }
+}
+
+/*
+ Output:
+I am learning Java
+I am practising Java
+Tomorrow, I will teach Java
+ */
