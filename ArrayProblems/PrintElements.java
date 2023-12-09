1b21cea8a7c5c5ee923e3d8a602f65723e59e82f
C Karthikesavan <karthikesavan2049@gmail.com>
2023-12-09 23:11:38 +0530

    Day 14

---
+++ ArrayProblems/PrintElements.java
@@ -0,0 +1,12 @@
+
+public class PrintElements {
+    public static void main(String[] args) {
+        
+        int[] arr = {10,20,30,40,5060,70,80,90,100};
+        // Printing the array elements using for loop iteration
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
