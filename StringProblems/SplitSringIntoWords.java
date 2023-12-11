96dd7c200d16e36ff294adc200cba3852eb742e5
C Karthikesavan <karthikesavan2049@gmail.com>
2023-12-11 16:53:16 +0530

    Day 16

---
+++ StringProblems/SplitSringIntoWords.java
@@ -0,0 +1,14 @@
+package StringProblems;
+
+import java.util.Scanner;
+
+public class SplitSringIntoWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        for(String word : arr){
+            System.out.println(word);
+        }
+    }
+}
