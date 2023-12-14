63531eb98cdb623ab71db243581cf74ea4a0a4b8
C Karthikesavan <karthikesavan2049@gmail.com>
2023-12-14 23:35:27 +0530

    Day 19

--- StringProblems/PrintLargestWord.java
+++ StringProblems/PrintLargestWord.java
@@ -1,4 +1,4 @@
-package StringProblems;
+// package StringProblems;
 
 import java.util.Scanner;
 
@@ -6,9 +6,13 @@ public class PrintLargestWord {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String res = "",
-        for(int i = 0 ; i < str.length();i++){
-            res += str.toLowerCase();
+        String[] res = str.split(" ");
+        int size = Integer.MIN_VALUE;
+        for(int i = 0 ; i < res.length; i++){
+            if(res[i].length() > size){
+                size = i;
+            }
         }
+        System.out.println("Longest word : "+res[size]);
     }
 }
