d1e9ba3b9e662697b51684f4ff286215db92c40e
cskk7 <karthikesavan2049@gmail.com>
2023-12-21 01:11:45 +0530

    Day 24

--- SeleniumTesting/GlobalSQA/src/Main.java
+++ SeleniumTesting/GlobalSQA/src/Main.java
@@ -1,9 +1,9 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.lang.*;
 public class Main {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-
+    public static void main(String[] args) throws InterruptedException{
+        HomePage home = new HomePage();
+        home.homePage();
     }
 }
