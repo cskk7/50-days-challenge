abc539c5457c9e918848560584c5854d0644d72d
cskk7 <karthikesavan2049@gmail.com>
2023-12-21 00:43:25 +0530

    Day 24

--- SeleniumTesting/GlobalSQA/src/HomePage.java
+++ SeleniumTesting/GlobalSQA/src/HomePage.java
@@ -1,2 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
+    public void homePage(String homePageURL){
+        WebDriver driver = new ChromeDriver();
+        driver.get(homePageURL);
+
+    }
 }
